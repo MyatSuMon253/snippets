3f62782199e96d0e9b7c5ccb8ebe7757ae9848f4
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-06-26 08:58:55 +0630

    feat: use useFormState from react-dom and add form validation

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -17,3 +17,35 @@ export async function deleteSnippet(id: number) {
   });
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  // check the user's inputs and make sure they are valid
+  const title = formData.get("title");
+  const code = formData.get("code");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return {
+      message: "Title must be longer",
+    };
+  }
+
+  if (typeof code !== "string" || code.length < 10) {
+    return {
+      message: "Code must be longer",
+    };
+  }
+
+  // create a new record in the database
+  const snippet = await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  // redirect the user back to the root route
+  redirect("/");
+}
--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,29 +1,15 @@
-import { db } from "@/app/db";
-import { notFound, redirect } from "next/navigation";
+"use client";
+
+import { useFormState } from "react-dom";
+import * as actions from "../../actions";
 
 const SnippetCreatePage = () => {
-  async function createSnippet(formData: FormData) {
-    // this needs to be a server action
-    "use server";
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
 
-    // check the user's inputs and make sure they are valid
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    // crate a new record in the database
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    // redirect the user back to the root route
-    redirect("/");
-  }
-  
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -46,6 +32,11 @@ const SnippetCreatePage = () => {
             id="code"
           />
         </div>
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        ) : null}
         <button type="submit" className="border rounded p-2 bg-blue-200">
           Create
         </button>
